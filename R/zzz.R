ef5c459cff9363b6158daa8457d7070fc02dbb8d
Ian Lyttle <ijlyttle@me.com>
2022-04-30 12:31:05 -0500

    add description to memoise

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,17 @@
 .onLoad <- function(libname, pkgname) {
+
+  # https://r-pkgs.org/r.html#when-you-do-need-side-effects
+  #
+  # .onLoad() is used to manage side-effects when your package is loaded.
+  #
+  # https://memoise.r-lib.org/reference/memoise.html
+  #
+  # The side-effect we're using here is to memoise uss_get_games():
+  #
+  # Whenever we call the memoised version, it:
+  #  - keeps track of the input arguments
+  #  - caches the result
+  #  - returns the cached result whenever we call using the same input args
+
   uss_get_games <<- memoise::memoise(uss_get_games)
 }
