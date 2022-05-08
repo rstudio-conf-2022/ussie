6ae2670d98e9ff9fce1b84ffd379db797254be91
Ian Lyttle <ijlyttle@users.noreply.github.com>
2022-05-08 12:38:16 -0500

    change games to matches (#3)
    * Add NEWS.md
    
    * change "games" to "matches"

---
+++ NEWS.md
@@ -0,0 +1,5 @@
+# ussie 0.0.0.9000
+
+* Changed `_games` to `_matches`, e.g. `uss_make_matches()`.
+
+* Added a `NEWS.md` file to track changes to the package.
--- R/columns.R
+++ R/columns.R
@@ -2,7 +2,7 @@ cols_engsoc <- function() {
   c("Date", "Season", "home", "visitor", "hgoal", "vgoal", "tier")
 }
 
-cols_games <- function() {
+cols_matches <- function() {
   c("country", "tier", "season", "date", "home",
     "visitor", "goals_home", "goals_visitor")
 }
