5ccfd469be6807906fe1e18ccfb912d9e5dea59f
Ian Lyttle <ijlyttle@me.com>
2022-05-15 12:59:25 -0500

    get plot looking "good"

--- R/plot.R
+++ R/plot.R
@@ -15,7 +15,14 @@ uss_plot_seasons_tiers <- function(data_seasons, aes_y = .data$wins, ncol = 1) {
       ggplot2::aes(
         x = .data$season,
         y = {{ aes_y }}
-      )
+      ),
+      color = "#333333"
+    ) +
+    # force() is a base function
+    # see: https://github.com/tidyverse/ggplot2/issues/4511#issuecomment-866185530
+    ggplot2::scale_alpha_manual(
+      values = c(`1` = 0.1, `2` = 0.3, `3` = 0.5, `4` = 0.7),
+      limits = force
     ) +
     ggplot2::facet_wrap(ggplot2::vars(.data$team), ncol = ncol)
 
