793f12bc94d448c69a97461fc98c0bd5c7a7a03f
Ian Lyttle <ijlyttle@me.com>
2022-06-01 19:27:21 -0500

    update to 2.2.1

--- tests/testthat/test-matches.R
+++ tests/testthat/test-matches.R
@@ -3,11 +3,7 @@ test_that("uss_make_matches works", {
   italy <- uss_make_matches(engsoccerdata::italy, "Italy")
   
   expect_true(tibble::is_tibble(italy))
-  expect_named(
-    italy,   
-    c("country", "tier", "season", "date", "home", "visitor", 
-      "goals_home", "goals_visitor")
-  )
+  expect_named(italy, cols_matches())
   expect_identical(unique(italy$country), "Italy")
 
 })
