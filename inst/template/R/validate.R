7eab3c455d6df4e121d6d43f4f0db8717e6ba868
Ian Lyttle <ijlyttle@me.com>
2022-06-05 13:15:56 -0500

    add templated files

---
+++ inst/template/R/validate.R
@@ -0,0 +1,104 @@
+#' Validate object as data frame
+#'
+#' If `.data` is not a data frame, throw an error.
+#'
+#' @param .data object that inherits from `data.frame`
+#'
+#' @return Invisible `.data`, called for side-effects.
+#' @examples
+#' # success :)
+#' validate_data_frame(mtcars)
+#'
+#' # failure :(
+#' \dontrun{
+#'   validate_data_frame(3)
+#' }
+#'
+#' @noRd
+#'
+validate_data_frame <- function(.data, call = rlang::caller_env()) {
+  
+  # https://cli.r-lib.org/reference/cli_abort.html
+  # https://rlang.r-lib.org/reference/abort.html
+  #
+  # When managing an error, you'll need:
+  #  - a predicate
+  #    - specify the condition that that initiates the error
+  #  - information for user:
+  #    - message to print to screen, cli offers formatting tools
+  #  - information for developer:
+  #    - class: use pattern `<package>_error_<description>`
+  #    - other information:
+  #        - use to illuminate the predicate
+  #        - named items
+  #         - reserved: message, class, call, body, footer, trace, parent,
+  #                     use_cli_format
+  #  - if using from a validation-function (like this)
+  #    - use `call = rlang::caller_env()` to tell user about calling function
+  #    - return the "thing" you're checking, invisibly
+  #
+  # potential to update tidyverse design giude:
+  #  - https://design.tidyverse.org/err-constructor.html#error-hierarchies
+  
+  # predicate
+  if (!is.data.frame(.data)) {
+    cli::cli_abort(
+      # information for user
+      c(
+        "Must supply a data frame",
+        x = "You have supplied a {.cls {class(.data)}}."
+      ),
+      # class information for developers
+      class = "ussie_error_data",
+      # other information for developers
+      class_data = class(.data),
+      # tell user where we are calling from
+      call = call
+    )
+  }
+  
+  invisible(.data)
+}
+
+#  inherit documentation from other functions:
+#    https://roxygen2.r-lib.org/articles/rd.html#inheriting-documentation-from-other-topics
+
+#' Validate column-names of data frame
+#'
+#' If `.data` does not have columns, throw an error.
+#'
+#' @inherit validate_data_frame params return
+#' @param cols_req `character`, name(s) of required columns for `.data`
+#'
+#' @examples
+#' # success :)
+#' validate_cols(mtcars, cols_req = c("wt", "mpg"))
+#'
+#' # failure :(
+#' \dontrun{
+#'   validate_cols(mtcars, cols_req = c("bill_length_mm"))
+#' }
+#' @noRd
+#'
+validate_cols <- function(.data, cols_req, call = rlang::caller_env()) {
+  
+  cols_data <- names(.data)
+  cols_missing <- cols_req[!(cols_req %in% cols_data)]
+  
+  # predicate
+  if (length(cols_missing) > 0) {
+    
+    # helper function to format quantities
+    # - see https://cli.r-lib.org/articles/pluralization.html
+    qlen <- function(x) cli::qty(length(x))
+    
+    cli::cli_abort(
+      # information for user
+      # class information for developers
+      # other information for developers
+      # tell user where we are calling from
+    )
+  }
+  
+  invisible(.data)
+}
---
+++ inst/template/testthat/test-validate.R
@@ -0,0 +1,50 @@
+test_that("validate_data_frame works", {
+  
+  # ## 2.2.2 side effects (global state)
+  # ## use helper function to warn on partial matches
+  # ## - https://testthat.r-lib.org/articles/test-fixtures.html
+  
+  # ## 2.2.1 side effects (errors)
+  # ## test the `class` of the error, return error-condition object
+  # ##  - https://testthat.r-lib.org/reference/expect_error.html
+  error_condition <- expect_error(validate_data_frame(3), class = "ussie_error_data")
+  
+  # ## test the parts of error-condition object that you supply
+  expect_identical(error_condition$class_data, "numeric")
+  
+  # ## test the error-message using a snapshot
+  # ##  - https://testthat.r-lib.org/reference/expect_snapshot.html
+  # ##  - example output: https://github.com/ijlyttle/ussie/blob/main/tests/testthat/_snaps/validate.md
+  # expect_snapshot_error(validate_data_frame(3))
+  
+  # ## test the return object
+  # ##  - https://testthat.r-lib.org/reference/expect_invisible.html
+  out <- expect_invisible(validate_data_frame(mtcars))
+  expect_identical(out, mtcars)
+  
+})
+
+test_that("validate_cols works", {
+  
+  # ## 2.2.2 side effects (global state)
+  # ## use helper function to warn on partial matches
+ 
+  # ## 2.2.1 side effects (errors)
+  # ## test the `class` of the error, return error-condition object 
+  # error_condition <- expect_error(
+  #   validate_cols(mtcars, "foo"),
+  #   class = "ussie_error_cols"
+  # )
+  
+  # ## test the parts of error-condition object that you supply
+  # expect_identical(error_condition$cols_req, "foo")
+  # expect_identical(error_condition$cols_data, names(mtcars))
+ 
+  # ## test the error-message using a snapshot
+  # expect_snapshot_error(validate_cols(mtcars, "foo"))
+  
+  # ## test the return object
+  # out <- expect_invisible(validate_cols(mtcars, "mpg"))
+  # expect_identical(out, mtcars)
+})
+
